a24c0928c7cd47bfcbd4372f95cf27fc71d11702
ParadoxPD <ask.priyanshu@gmail.com>
2025-02-23 13:14:06 +0530

    Added some parsing capabilities

--- app/src/main/java/org/error/ErrorList.java
+++
@@ -1,9 +0,0 @@
-package org.error;
-
-public interface ErrorList {
-	String EOF = "EOF";
-	String TYPE_ERROR = "TYPE_ERROR";
-	String OUT_OF_TOKENS = "OUT_OF_TOKENS";
-	String INVALID_SYNTAX = "INVALID_SYNTAX";
-
-}
